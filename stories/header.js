342e0c1afbf888ee22bb135e891882d1e687f12f
Reniel Salvador <reniel.salvador@gmail.com>
2019-07-22 23:57:03 +0800

    Add sample stories

---
+++ stories/header.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import BaseInput from '../src/components/BaseInput'
+
+storiesOf('Header', module).add('with text', () => <BaseInput />)
